fcd619248bb4c4f3ef3e018923d6693fe1853dee
A-Frankenstein <brooke.s.mclean@gmail.com>
2020-01-28 15:25:28 -0800

    Create reverseString.py

---
+++ reverseString.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#reverse a string
+school = "well we had"
+school = school[::-1]
+#print(school)
+
+school = "all these children"
+school = list(school)
+school.reverse()
+school = "".join(school)
+#print(school)
